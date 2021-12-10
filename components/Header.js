2a30c7c50aadcd71cbd5f8a9d5ed674d380a5c1b
avneesh0612 <avneeshagarwal0612@gmail.com>
2021-12-10 16:47:17 +0530

    add custom scrollbar

--- components/Header.js
+++ components/Header.js
@@ -60,7 +60,7 @@ const Header = () => {
           Contact
         </a>
       </div>
-      <div className="flex">
+      <div className="hidden sm:flex">
         <SearchIcon className="w-5 h-5 mr-4" />
         <MoreIcon className="w-5 h-5 ml-4" />
       </div>
